87e61594823cae44412512d76f8c0495dae9bf6f
lollipopfly <lollipop.fly@gmail.com>
2016-11-14 12:35:02 +0300

    make auth

--- config/database.php
+++ config/database.php
@@ -1,5 +1,12 @@
 <?php
-
+if (getenv('DATABASE_URL')) {
+    $url = parse_url(getenv('DATABASE_URL'));
+    putenv("DB_HOST={$url['host']}");
+    putenv("DB_USERNAME={$url['user']}");
+    putenv("DB_PASSWORD={$url['pass']}");
+    $db = substr($url['path'],1);
+    putenv("DB_DATABASE={$db}");
+}
 return [
 
     /*
@@ -54,6 +61,7 @@ return [
 
         'mysql' => [
             'driver' => 'mysql',
+            'host' => env('DB_HOST', 'localhost'),
             'host' => env('DB_HOST', 'localhost'),
             'port' => env('DB_PORT', '3306'),
             'database' => env('DB_DATABASE', 'forge'),
